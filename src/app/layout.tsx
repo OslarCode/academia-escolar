5fb1fddf20e2da0528ed456d02764e0b8c46cb4e
OslarCode <oslar1980@gmail.com>
2024-09-11 12:29:39 +0200

    fix-colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,11 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>
-        <div className="flex h-screen bg-gray-100">
+        <div className="flex h-screen bg-slate-500">
           <Sidebar />
           <div className="flex-1 flex flex-col overflow-hidden">
             <Header />
-            <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+            <main className="flex-1 overflow-x-hidden overflow-y-auto bg-slate-500">
               <div className="container mx-auto px-6 py-8">{children}</div>
             </main>
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,28 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="space-y-6">
+    <div className="bg-slate-500 space-y-6">
       <h1 className="text-3xl font-bold">
         Bienvenido a la Gestión de Academia Escolar
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <Link
           href="/alumnos"
-          className="p-6 bg-white rounded-lg shadow hover:shadow-md transition-shadow"
+          className="p-6 bg-black rounded-lg shadow hover:shadow-md transition-shadow"
         >
           <h2 className="text-xl font-semibold mb-2">Gestión de Alumnos</h2>
           <p>Administra la información de los estudiantes.</p>
         </Link>
         <Link
           href="/profesores"
-          className="p-6 bg-white rounded-lg shadow hover:shadow-md transition-shadow"
+          className="p-6 bg-black rounded-lg shadow hover:shadow-md transition-shadow"
         >
           <h2 className="text-xl font-semibold mb-2">Gestión de Profesores</h2>
           <p>Administra la información de los profesores.</p>
         </Link>
         <Link
           href="/cursos"
-          className="p-6 bg-white rounded-lg shadow hover:shadow-md transition-shadow"
+          className="p-6 bg-black rounded-lg shadow hover:shadow-md transition-shadow"
         >
           <h2 className="text-xl font-semibold mb-2">Gestión de Cursos</h2>
           <p>Administra los cursos disponibles.</p>
